4087a50781083482904b8882b8f6350f551c034a
Maksat Madeniyetov <69766509+maksat-madeniyetov@users.noreply.github.com>
2022-05-13 17:49:18 +0600

    Create 36. Valid Sudoku.java

---
+++ 36. Valid Sudoku.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                
+                if (board[i][j] != '.') {
+                    
+                    if (set.contains("row" + i + board[i][j]) ||
+                       set.contains("col" + j + board[i][j]) || 
+                       set.contains("square" + i/3 + j/3 + board[i][j])) {
+                        return false;
+                    }
+                    
+                    set.add("row" + i + board[i][j]);
+                    set.add("col" + j + board[i][j]);
+                    set.add("square" + i/3 + j/3 + board[i][j]);
+                }
+                
+            }
+        }
+        return true;
+    }
+}
